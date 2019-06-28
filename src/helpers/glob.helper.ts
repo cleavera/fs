501054fa664f6903039b2c921dba470b18f60156
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2019-06-28 11:29:59 +0100

    Tidy up linting rules

--- src/helpers/glob.helper.ts
+++ src/helpers/glob.helper.ts
@@ -1,9 +1,10 @@
+import { $isNull, Maybe } from '@cleavera/utils';
 import * as globMatcher from 'glob';
 
 export function $glob(pattern: string): Promise<Array<string>> {
     return new Promise<Array<string>>((resolve: (matches: Array<string>) => void, reject: (error: Error) => void ): void => {
-        globMatcher(pattern, { cwd: process.cwd() }, (err: Error, matches: Array<string>) => {
-            if (err) {
+        globMatcher(pattern, { cwd: process.cwd() }, (err: Maybe<Error>, matches: Array<string>) => {
+            if (!$isNull(err)) {
                 reject(err);
 
                 return;
